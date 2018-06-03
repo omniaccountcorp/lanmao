e5db3d904f7a4d0844349d6fda377e759c13d5d1
lsaoe <fantalsaoe@gmail.com>
2018-06-03 21:59:52 +0800

    修改spec

--- spec/api/trade/establish_project_spec.rb
+++ spec/api/trade/establish_project_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe "创建标的" do
   let(:annnual_interest_rate) { 0.15 }
   let(:project_period) { 90 }
 
-  it "创建成功" do
+  it "创建失败" do
     res = client.establish_project(request_no, user_no, project_no, project_amount, project_name, project_type, annnual_interest_rate, repayment_way, project_period)
 
     expect(res[:result]).not_to eq('S')
