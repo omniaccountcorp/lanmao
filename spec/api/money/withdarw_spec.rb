ad359e28bdf7453754c42f24258bf8de3a37a0a8
lsaoe <fantalsaoe@gmail.com>
2018-06-03 21:03:33 +0800

    提现

---
+++ spec/api/money/withdarw_spec.rb
@@ -0,0 +1,16 @@
+# coding: utf-8
+require 'spec_helper'
+
+RSpec.describe '自动提现' do
+  let(:user_no) { '1' }
+  let(:flow_id) { Lanmao::Utils.gen_flow_id }
+  let(:withdraw_type) { "NORMAL" }
+  let(:amount) { 500 }
+  let(:commission) { nil }
+
+  it '失败' do
+    res = client.withdraw(user_no, flow_id, withdraw_type, amount, commission)
+
+    expect(res[:result]).not_to eq "S"
+  end
+end
