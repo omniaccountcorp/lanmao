963aedad5b9f525f802f7b39419b412e089b9496
peterswj <peterswj@163.com>
2018-06-05 11:34:08 +0800

    取消用户授权API

--- spec/api/trade/cancel_debenture_sale_spec.rb
+++ spec/api/trade/cancel_debenture_sale_spec.rb
@@ -1,15 +1,16 @@
 # coding: utf-8
 require 'spec_helper'
 
-RSpec.describe "取消债权出让" do
+RSpec.describe "取消用户授权" do
   let(:request_no) { Lanmao::Utils.gen_flow_id }
-  let(:no) { "123" }
+  let(:platform_user_no) { "123" }
+  let(:auth_list) {"TENDER"}
 
-  it "失败" do
-    res = client.cancel_debenture_sale(request_no, no)
+  it "请求成功，用户不存在" do
+    res = client.cancel_user_authorization(request_no, platform_user_no, auth_list)
 
     # ap res
-    expect(res[:data]["errorMessage"]).to include"债权出让订单不存在"
+    expect(res[:data]["errorMessage"]).to include"用户不存在"
     expect(res[:result]).not_to eq('S')
   end
 end
