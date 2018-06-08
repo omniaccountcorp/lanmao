f1eaa65aa5b48017e536edb369f6dc31156367bb
peterswj <peterswj@163.com>
2018-06-08 10:47:52 +0800

    fix query_user_information spec

--- spec/api/query/query_user_information_spec.rb
+++ spec/api/query/query_user_information_spec.rb
@@ -3,10 +3,11 @@ require 'spec_helper'
 
 RSpec.describe '用户信息查询' do
   it "查询失败" do
-    params = {platformUserNo: 'c02'}
+    res = client.query_user_information('98888')
 
-    res = client.query_user_information('c01')
-
-    expect(res[:result]).to eq('S')
+    # ap res
+    expect(res[:data][:userType]).to eq "PERSONAL"
+    expect(res[:data][:bankcode]).to eq "ICBK"
+    expect(res[:result]).to eq("S")
   end
 end
